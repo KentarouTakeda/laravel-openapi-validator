6dcaf564613cdd5c88261ad085f22e79991fc078
武田 憲太郎 <takeda@youmind.jp>
2023-12-19 22:15:52 +0900

    php-cs-fixer設定

---
+++ .php-cs-fixer.php
@@ -0,0 +1,20 @@
+<?php
+return (new PhpCsFixer\Config())
+    ->setRiskyAllowed(true)
+    ->setRules([
+        '@PSR1' => true,
+        '@PSR2' => true,
+        '@Symfony' => true,
+        'php_unit_method_casing' => false,
+        'phpdoc_summary' => false,
+        'phpdoc_align' => false,
+        'phpdoc_separation' => false,
+        'phpdoc_no_alias_tag' => false,
+        'phpdoc_to_comment' => false,
+        'no_trailing_whitespace_in_comment' => false,
+    ])
+    ->setFinder(PhpCsFixer\Finder::create()
+        ->in(__DIR__.'/src')
+        ->in(__DIR__.'/tests')
+    )
+;
