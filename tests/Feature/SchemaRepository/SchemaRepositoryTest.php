abcea389f636c1069de6c8916b8ad69bc368568d
武田 憲太郎 <takeda@youmind.jp>
2023-12-30 19:08:15 +0900

    Add L5SwaggerResolver test to SchemaRepositoryTest

--- tests/Feature/SchemaRepository/SchemaRepositoryTest.php
+++ tests/Feature/SchemaRepository/SchemaRepositoryTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace KentarouTakeda\Laravel\OpenApiValidator\Tests\Feature\SchemaRepository;
 
+use KentarouTakeda\Laravel\OpenApiValidator\SchemaRepository\L5SwaggerResolver;
 use KentarouTakeda\Laravel\OpenApiValidator\SchemaRepository\LaravelOpenApiResolver;
 use KentarouTakeda\Laravel\OpenApiValidator\SchemaRepository\SchemaRepository;
 use KentarouTakeda\Laravel\OpenApiValidator\Tests\Feature\TestCase;
@@ -25,5 +26,21 @@ class SchemaRepositoryTest extends TestCase
         ]);
 
         $laravelOpenApiResolver->shouldHaveReceived('getJson')->once();
+    }
+
+    /**
+     * @test
+     */
+    public function L5SwaggerResolverIsUsed(): void
+    {
+        $l5SwaggerResolver = $this->mock(L5SwaggerResolver::class, function ($mock) {
+            $mock->shouldReceive('getJson')->andReturn('{"foo":"bar"}');
+        });
+
+        app()->makeWith(SchemaRepository::class, [
+            'providerName' => 'l5-swagger',
+        ]);
+
+        $l5SwaggerResolver->shouldHaveReceived('getJson')->once();
     }
 }
