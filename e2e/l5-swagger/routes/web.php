cf82da8f45469d1dae998c3244bf862facf27afe
武田 憲太郎 <takeda@youmind.jp>
2025-12-19 22:32:27 +0900

    Update Laravel to v12 for e2e tests

--- e2e/l5-swagger/routes/web.php
+++ e2e/l5-swagger/routes/web.php
@@ -4,16 +4,5 @@ use App\Http\Controllers\ExampleController;
 use Illuminate\Support\Facades\Route;
 use KentarouTakeda\Laravel\OpenApiValidator\Http\Middleware\OpenApiValidator;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
 Route::get('/', ExampleController::class)
     ->middleware(OpenApiValidator::class);
